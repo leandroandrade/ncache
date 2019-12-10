8eaf0ab469063a73eae31fe0baf4ca79db424538
leandroandrade <leandro.andrade2588@gmail.com>
2019-12-10 13:28:15 -0300

    Logs no repositorio de carros

--- src/repository/carros.js
+++ src/repository/carros.js
@@ -5,10 +5,14 @@ const { getConnection, closeConnection } = require('../database/mysql');
 exports.getPelaPlaca = async placa => {
     const connection = await getConnection();
 
+    console.log(`MYSQL: pesquisando placa ${ placa }`);
+
     const [data] = await connection.query({
         sql: 'SELECT * FROM carros WHERE placa = ?',
         timeout: 40000, // 40s
     }, placa);
+
+    console.log(`MYSQL: placa ${ placa } retornada do banco de dados!`);
 
     closeConnection(connection);
 
