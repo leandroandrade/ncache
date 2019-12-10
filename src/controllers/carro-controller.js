0cdf3cef6904c2856b0723a2f817832e921803c2
leandroandrade <leandro.andrade2588@gmail.com>
2019-12-10 13:16:54 -0300

    Busca de placa pelo cache

--- src/controllers/carro-controller.js
+++ src/controllers/carro-controller.js
@@ -1,15 +1,25 @@
 'use strict';
 
 const carros = require('../repository/carros');
+const { getCache, setCache } = require('../database/redis');
+
+const expireSeconds = 30;
+const key = placa => `carros:${ placa }`;
 
 const getCarroPelaPlaca = async (req, res, next) => {
     const { placa } = req.params;
 
-    const carro = await carros.getPelaPlaca(placa);
+    const keyPlaca = key(placa);
 
+    const cached = await getCache(keyPlaca);
+    if (cached) return res.json(cached);
+
+    const carro = await carros.getPelaPlaca(placa);
     if (!carro) return res
         .status(404)
         .send({ message: `Carro com placa ${ placa } nao encontrado!` });
+
+    await setCache(keyPlaca, carro, expireSeconds);
 
     return res.json(carro);
 };
--- src/database/redis.js
+++ src/database/redis.js
@@ -8,13 +8,13 @@ const client = new Redis({ keyrefix: 'cache:' });
 const getCache = async (key) =>
     client.get(key)
           .timeout(2000)
-          .then(cache => cache ? JSON.parse(cache) : null)
+          .then(cache => cache ? (console.log(`REDIS: dados obtido do cache!`), JSON.parse(cache)) : null)
           .catch(err => console.log('ERROR_REDIS: Timeout excedido getCache'));
 
 const setCache = async (key, value, expireSeconds) =>
     client.set(key, JSON.stringify(value), 'EX', expireSeconds)
           .timeout(2000)
-          .then(() => console.log(`REDIS: ${ key } adicionada no REDIS`))
+          .then(() => console.log(`REDIS: key ${ key } adicionada!`))
           .catch(err => console.log('ERROR_REDIS: Timeout excedido setCache'));
 
 module.exports = { getCache, setCache };
