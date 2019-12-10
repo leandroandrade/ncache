5617b233351df2b918dd70820b2e7fb3df93e85d
leandroandrade <leandro.andrade2588@gmail.com>
2019-12-10 13:59:01 -0300

    Adicionado teste de integracao

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const { version } = require('../../package.json');
 
-const { getCarroPelaPlaca } = require('../controllers/carro-controller');
+const { getCarroPelaPlaca } = require('../controllers/carros-controller');
 
 router.get('/', (req, res) => res.json({
     title: "ncache working",
